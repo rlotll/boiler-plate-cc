3e1275adbeb048dca5c5b0fd241ed4bf152a7ff2
rlotll <rlarmdql@gmail.com>
2026-03-12 14:33:54 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+const config = require('./config/key')
+
+//application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true}));
+//application/json
+app.use(express.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://rlotll:rmdql_0026m@boilerplate.wc1px37.mongodb.net/?appName=boilerplate', {}).then(() => console.log('MongoDB Connected...'))
+mongoose.connect('config.mongoURI', {}).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body)
+
+  user.save().then(() => {
+    res.status(200).json({
+      success: true
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+    res.json({
+      success: false,
+      err: err
+    });
+  });
+
 })
 
 app.listen(port, () => {
